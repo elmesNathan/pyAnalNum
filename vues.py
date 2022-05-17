5a0752ac5a46605cf0e028d50550dd59824739d2
elmesNathan <electromecatronique01@gmail.com>
2022-05-17 13:23:46 +0200

    Vue résultat de l'intégrale par la méthode du rectangle

--- vues.py
+++ vues.py
@@ -2,16 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """
-Ce fichier nommer back_controleur permets de faire le calcul
-numérique.
+Ce fichier nommer vues permet d'intérargir avec l'utilisateur,
 
-Pour ce faire, il rappatrie les données provenant du fichier 
-front_controleur et les formules du fichier models.
-
-En suite il évalue le résultat en faisant correspondre les données 
-et la formules correspondant aux données.
-
-Le résultat est directement envoyer au fichier vues
 Auteur
 -------
 Elmes
@@ -19,50 +11,25 @@ electromecatronique01@gmail.com
 
 Date de création
 -----------------
-17/Mai/2022 12:00
+17/Mai/2022 13:00
 """
-
 #Importation
-
-from models import *
-from front_controleurs import *
-from vues import *
+from numpy as np
+from matplotlib.pyplot as plt
 
 #Définitions
-def methonde_rectagles(a, b, n, f, choix):
-	"""
-	Calcul de l'intégrale par la méthode du Rectagle
-	On définit h par :
-		 b - a
-	h = -------
-	       n	
-	On trouve les abscices X par :
-	X = xi (Pour la méthode du Rectangle à Gauche)
-	X = xi + h (Pour la méthode du Rectangle à Droite)
+def vue_rectangle(xi, yi, solution):
+	x = np.array(xi)
+	y = np.array(yi)
+	plt.plot(x, y, label="Val approximative de f dans xn - xi")
+	plt.xlabel("Discretisation avec écart h")
+	plt.ylabel("Approximation de f")
+	plt.title("Proximation de l'intégrale par la méthode du Rectangle")
 
-	On trouve les ordonnées Y par :
-	Y = yi
+	plt.show()
 
-	Et enfin, on détermine la valeur approchée en 
-	évaluant la formule de l'approximation par le rectangle.
-	"""
-	h = (b - a)/n
-	xi = []
-	yi = []
-
-	if choix == "G":
-		for x in range(a,b,h):
-			yi.append(eval(f))
-	else:
-		for x in range(a+h, b+h, h):
-			yi.append(eval(f))
-
-	solution = eval(rectagles())
-
-	return vue_rectangle(xi, yi, solution)
 
 
 #Affectation
 
 #if __name__ == '__main__' :
-
